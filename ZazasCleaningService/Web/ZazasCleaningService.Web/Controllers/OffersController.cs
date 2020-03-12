53e70407d4d6731c37ef8cc82d31ccc556d84c95
Tihomir Ivanov <tihomir_2005@abv.bg>
2020-03-12 20:05:09 +0200

    Add offers controller and view

---
+++ ZazasCleaningService/Web/ZazasCleaningService.Web/Controllers/OffersController.cs
@@ -0,0 +1,12 @@
+﻿namespace ZazasCleaningService.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class OffersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
