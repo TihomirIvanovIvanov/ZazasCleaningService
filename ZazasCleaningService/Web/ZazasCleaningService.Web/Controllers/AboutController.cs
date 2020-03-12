11000144e17fd74a993b3b1aea86bacb86147419
Tihomir Ivanov <tihomir_2005@abv.bg>
2020-03-12 19:33:05 +0200

    Add about controller and view

---
+++ ZazasCleaningService/Web/ZazasCleaningService.Web/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿namespace ZazasCleaningService.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class AboutController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
