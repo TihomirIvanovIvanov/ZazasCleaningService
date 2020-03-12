6c9eecd54c9758bb848079f6441ce34ee1c649fb
Tihomir Ivanov <tihomir_2005@abv.bg>
2020-03-12 19:49:29 +0200

    Add contact controller and view

---
+++ ZazasCleaningService/Web/ZazasCleaningService.Web/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿namespace ZazasCleaningService.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ContactController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
