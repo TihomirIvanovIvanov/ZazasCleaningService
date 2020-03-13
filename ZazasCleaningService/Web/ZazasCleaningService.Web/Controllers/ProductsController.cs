42594a1d917a806824f4251ab262a84dcf798995
Tihomir Ivanov <tihomir_2005@abv.bg>
2020-03-13 16:44:07 +0200

    Add product controller and view

---
+++ ZazasCleaningService/Web/ZazasCleaningService.Web/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿namespace ZazasCleaningService.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ProductsController : Controller
+    {
+        public IActionResult All()
+        {
+            return this.View();
+        }
+    }
+}
