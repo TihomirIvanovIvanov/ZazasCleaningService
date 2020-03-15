a5168c2fadf73a8c967f66db31f8b212d7686927
Tihomir Ivanov <tihomir_2005@abv.bg>
2020-03-15 17:47:53 +0200

    Add scaffold identity account logout

---
+++ ZazasCleaningService/Web/ZazasCleaningService.Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,27 @@
+﻿namespace ZazasCleaningService.Web.Areas.Identity.Pages.Account
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using ZazasCleaningService.Data.Models;
+
+    [AllowAnonymous]
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public LogoutModel(SignInManager<ApplicationUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            await this.signInManager.SignOutAsync();
+            return this.Redirect("/Identity/Account/Login");
+        }
+    }
+}
