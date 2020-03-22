4b6f480df65b6fd8d8f3684c11c0d5a1391b6147
tihomir <tihomir_2005@abv.bg>
2020-03-22 16:55:57 +0200

    Configure one to one relation for order and receipt. Add user role and make admin on register the first user.

---
+++ ZazasCleaningService/Data/ZazasCleaningService.Data/Configurations/ReceiptConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace ZazasCleaningService.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using ZazasCleaningService.Data.Models;
+
+    public class ReceiptConfiguration : IEntityTypeConfiguration<Receipt>
+    {
+        public void Configure(EntityTypeBuilder<Receipt> receipt)
+        {
+            receipt
+                .HasOne(x => x.Order)
+                .WithOne(x => x.Receipt)
+                .HasForeignKey<Receipt>(x => x.OrderId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
