6d1b0d2b99a704ead3340c1f4e17388357ebb494
tihomir <tihomir_2005@abv.bg>
2020-04-03 16:35:31 +0300

    Add services controller and create view

---
+++ ZazasCleaningService/Web/ZazasCleaningService.Web/Areas/Administration/Controllers/ServicesController.cs
@@ -0,0 +1,12 @@
+﻿namespace ZazasCleaningService.Web.Areas.Administration.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ServicesController : AdministrationController
+    {
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+    }
+}
