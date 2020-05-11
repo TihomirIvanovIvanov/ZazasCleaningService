4559ad73647cc2acda70e59c5638af39e32e6d4c
tihomir <tihomir_2005@abv.bg>
2020-05-11 15:50:29 +0300

    Add product orders property in Receipt entity

--- ZazasCleaningService/Data/ZazasCleaningService.Data.Models/Receipt.cs
+++ ZazasCleaningService/Data/ZazasCleaningService.Data.Models/Receipt.cs
@@ -1,13 +1,22 @@
 ﻿namespace ZazasCleaningService.Data.Models
 {
+    using System.Collections.Generic;
+
     using ZazasCleaningService.Data.Common.Models;
 
     public class Receipt : BaseDeletableModel<int>
     {
+        public Receipt()
+        {
+            this.ProductOrders = new List<ProductOrder>();
+        }
+
         public string IssuedOnPicture { get; set; }
 
         public string RecipientId { get; set; }
 
         public virtual ApplicationUser Recipient { get; set; }
+
+        public List<ProductOrder> ProductOrders { get; set; }
     }
 }
